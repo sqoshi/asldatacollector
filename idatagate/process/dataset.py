61663149e4d599df09021ca92ec453dc57c2ef57
Piotr Popis <piotr.popis@nokia.com>
2024-09-08 22:56:46 +0200

    progress bar on processing

--- idatagate/process/dataset.py
+++ idatagate/process/dataset.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import pickle
 import random
@@ -35,7 +36,7 @@ def process_all(
     output: str = "data.pickle",
     batch: int = 100,
 ):
-    """Process all classes across all sample directories in batches."""
+    """Process all classes across all sample directories in batches with progress tracking."""
     hands = Hands(
         max_num_hands=1,
         static_image_mode=True,
@@ -44,16 +45,27 @@ def process_all(
     result = {"data": [], "labels": []}
     all_images = [str(file) for file in Path(dir).rglob("*") if file.is_file()]
     random.shuffle(all_images)
-    for img_path in all_images:
+
+    total_images = len(all_images)
+    valid_count = 0
+    progress_interval = total_images // 10
+
+    for i, img_path in enumerate(all_images, 1):
         data_aux, valid = process_image(hands, img_path)
         if valid:
             label = img_path.split(os.path.sep)[-2]
             result["data"].append(data_aux)
             result["labels"].append(label)
+            valid_count += 1
 
-    # # Display stats after processing
-    # unique_labels, counts = np.unique(all_labels, return_counts=True)
-    # print_class_stats(unique_labels, counts)
-    # draw_class_bar_plot(unique_labels, counts)
+        if i % progress_interval == 0 or i == total_images:
+            logging.info(
+                f"Processed {i} out of {total_images} images ({(i / total_images) * 100:.0f}%)"
+            )
+
+    logging.info(
+        f"{valid_count} out of {total_images} images were valid ({(valid_count / total_images) * 100:.2f}%)"
+    )
+
     with open(output, "wb") as f:
         pickle.dump(result, f)
